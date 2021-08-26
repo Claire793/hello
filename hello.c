e32a309bde2de49ae2865db02408086015d62d6e
Claire Anderson <canderson110@huskers.unl.edu>
2021-08-26 22:56:13 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Claire Anderson and Hannah Shin
+ * Date: 2021/08/24
+ * 
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Hello World! I'm Claire Anderson\n");
+     printf("Computer Science\n");
+
+     return 0;
+     
+ }
